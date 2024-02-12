e20af474667dde6baa4b52c4339a07f23682489e
Nguyễn Đức Lai <32266083+nguyenduclai99@users.noreply.github.com>
2024-02-12 04:08:21 +0000

    update

--- tuongtacmangxahoi.js
+++ tuongtacmangxahoi.js
@@ -93,7 +93,7 @@ document.addEventListener('DOMContentLoaded', function() {
       getPaymets();
     }
 
-    if (pathname = '/') {
+    if (pathname == '/') {
       getNotificantions();
     }
   }, 700);
