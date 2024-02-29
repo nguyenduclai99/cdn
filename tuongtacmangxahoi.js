e54c69da9edf853194c03eed87d44cd8918f0b8b
Nguyễn Đức Lai <32266083+nguyenduclai99@users.noreply.github.com>
2024-02-29 09:55:16 +0000

    Update

--- tuongtacmangxahoi.js
+++ tuongtacmangxahoi.js
@@ -7,7 +7,7 @@ const isURL = (str) => {
 }
 
 const createLogs = async () => {
-  let referrer = isURL(document.referrer) && (new URL(url)).hostname == 'tuongtacmangxahoi.io.vn' ? '': document.referrer;
+  let referrer = isURL(document.referrer) && (new URL(document.referrer)).hostname == 'tuongtacmangxahoi.io.vn' ? '': document.referrer;
 
   let data = {
     end_point: `${window.location.pathname}?referrer=${referrer}`,
